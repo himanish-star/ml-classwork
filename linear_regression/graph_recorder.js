8e92cd5c71f9a163f5ec513ec3e106f9080a2a93
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2019-01-11 23:18:33 +0530

    added more fixes

--- linear_regression/graph_recorder.js
+++ linear_regression/graph_recorder.js
@@ -4,8 +4,31 @@ window.onload = function () {
 
     const {left, top} = canvas.getBoundingClientRect();
 
+    let points = [];
+
     document.getElementById('graph').onclick = e => {
-      ctx.strokeRect(e.x-left,e.y-top,1,1);
+      ctx.strokeRect(e.x-left,e.y-top,5,5);
+      points.push({x: e.x-left,y: e.y-top});
       console.log("x: " + e.x,"y: " + e.y,"left -> " + left,"top -> " + top);
+      drawLine(points);
     };
 };
+
+const drawLine = (points) => {
+  console.log(points);
+  let sumX = 0, sumY = 0;
+  points.forEach(point => {
+    sumX += point.x;
+    sumY += point.y;
+  });
+  const meanX = sumX/points.length;
+  const meanY = sumY/points.length;
+  console.log(meanX, meanY);
+};
+
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+// ctx.beginPath();
+// ctx.moveTo(0, 0);
+// ctx.lineTo(300, 150);
+// ctx.stroke();
