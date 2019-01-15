0dbd82b16c0ab857b486051508f49e314d2171be
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2019-01-15 18:56:40 +0530

    residual plot done

--- linear_regression/nodejs/graph_recorder.js
+++ linear_regression/nodejs/graph_recorder.js
@@ -1,8 +1,13 @@
 window.onload = function () {
   const canvas = document.getElementById("graph");
+  const canvas2 = document.getElementById("graph2");
   const ctx = canvas.getContext("2d");
+  const ctx2 = canvas2.getContext("2d");
 
   const {left, top, width, height} = canvas.getBoundingClientRect();
+  const {left: left2, top: top2, width: width2, height: height2} = canvas2.getBoundingClientRect();
+
+  console.log(canvas2.getBoundingClientRect());
 
   let points = [];
 
@@ -10,13 +15,26 @@ window.onload = function () {
     clearCanvas(ctx,width,height);
     points.push({x: e.x-left,y: e.y-top});
     plotPoints(ctx,points);
-    if(points.length > 1)
-      drawLine(points,left+width,top+height);
+    if(points.length > 1) {
+      const {beta0,beta1} = drawLine(points,left+width,top+height);
+      const newPoints = residualPlot(ctx2,points,beta0,beta1);
+      console.log(newPoints);
+      plotResidualPoints(ctx2,newPoints,left2,top2,width2,height2);
+    }
   };
 
   // window.setTimeout(() => {
   //   clearCanvas(ctx,width,height);
   // },2000);
+
+};
+
+const plotResidualPoints = (ctx,newPoints,left,top,width,height) => {
+  ctx.clearRect(0,0,width,height);
+  newPoints.forEach(point => {
+    ctx.strokeRect(point.yexp/2,height/2+point.yexp/2-point.yact/2,5,5);
+    console.log(point.yexp,point.yexp-point.yact);
+  });
 };
 
 const plotPoints = (ctx,points) => {
@@ -27,6 +45,16 @@ const plotPoints = (ctx,points) => {
 
 const clearCanvas = (ctx,width,height) => {
   ctx.clearRect(0,0,width,height);
+};
+
+const residualPlot = (ctx,points,beta0,beta1) => {
+  let newPoints = [];
+  points.forEach(point => {
+    const yExpected = point.y;
+    const yActual = beta0 + (beta1*point.x);
+    newPoints.push({yexp: yExpected,yact: yActual});
+  });
+  return newPoints;
 };
 
 const drawLine = (points,endBottom,endRight) => {
@@ -61,4 +89,5 @@ const drawLine = (points,endBottom,endRight) => {
   ctx.moveTo(meanX,meanY);
   ctx.lineTo(newX, endBottom);
   ctx.stroke();
+  return {beta0,beta1};
 };
---
+++ linear_regression/python/graph_recorder.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    
+
+if __name__ == 'main':
+    main()
