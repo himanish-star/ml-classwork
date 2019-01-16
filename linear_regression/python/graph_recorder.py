92dc173d1a94ad39392bdf9a2f79a0bfe8bcec21
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2019-01-16 13:37:57 +0530

    python_requirements

--- linear_regression/python/graph_recorder.py
+++ linear_regression/python/graph_recorder.py
@@ -1,8 +1,14 @@
 import numpy as np
+import pandas as pd
+import csv
 import matplotlib.pyplot as plt
 
+""" code for plotting Anscombe's datasets"""
+
 def main():
-    
+    with open('anscombes dataset.csv') as csv_file:
+        csv_reader = csv.read(csv_file, delimiter = ',')
+        print(csv_reader)    
 
 if __name__ == 'main':
     main()
