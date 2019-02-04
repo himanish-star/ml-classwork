5b73690235faa7d9baf7f88417909767e85fc2ec
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2019-02-04 15:08:11 +0530

    ID 3 completely implemented

--- id3_algo/id3_algo.py
+++ id3_algo/id3_algo.py
@@ -1,15 +1,33 @@
 import math, os, pandas as pd, numpy as np, matplotlib.pyplot as plt
+from graphviz import Digraph
 
 '''Entropy(s) = - summation(p(log(p)))'''
 '''Gain(s,a)  = Entropy(s) - summation(p(s/a)*Entropy(s/a))'''
+
+dot = Digraph(comment = 'ID3 algo')
+dot.format = 'png'
+
+global_edges = []
+special_edges = []
+
+def plotTree(state, res):
+    nodes = state.split()
+    if not len(nodes):
+        return
+    nodes = [ nodes[i].split('=') for i in range(len(nodes)) if i%2]
+    for i in range(len(nodes)-1):
+        global_edges.append([nodes[i][0],nodes[i+1][0],nodes[i][1]])
+    special_edges.append([nodes[-1][0],res,nodes[-1][1]])
 
 def train(previous, attributes, dt, state):
     EntropyP = calc_entropy(previous)
     if not len(dt) or EntropyP == 0:
         if not previous[0]:
             print('AND'.join(state.split('AND')[:-1]), 'THEN play=no')
+            plotTree(state,'no')
         else:
             print('AND'.join(state.split('AND')[:-1]), 'THEN play=yes')
+            plotTree(state,'yes')
         return
     Gains = []
     total = sum(previous)
@@ -44,3 +62,17 @@ if __name__ == '__main__':
     systemPos = len(dataset[dataset['play']=='yes'])
     systemNeg = len(dataset[dataset['play']=='no'])
     train([systemPos, systemNeg], attributes, dataset, 'IF ')
+
+    global_edges = list(set([str(edge) for edge in global_edges]))
+
+    for edge in global_edges:
+        params = edge[1:-1].split(',')
+        p = [p.strip()[1:-1] for p in params]
+        dot.edge(p[0],p[1],label=p[2])
+
+    unique_counter = 0
+    for edge in special_edges:
+        unique_counter += 1
+        dot.node(str(unique_counter), edge[1])
+        dot.edge(edge[0],str(unique_counter),label=edge[2])
+    dot.render('id3-output.gv')
